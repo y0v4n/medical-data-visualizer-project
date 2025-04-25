ff32e8d555dec2839ae0b875d5169ab6014ae4f0
Jovan J. <j.j0v4n@pm.me>
2025-04-25 08:45:38 +0000

    Submitting my solution for the Medical Data Visualizer Project.

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,31 +4,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = ((df['weight'] / (df['height']/100)**2) > 25).astype(int)
 
 # 3
-
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(
+        df,
+        id_vars=['cardio'],
+        value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight']
+    )
 
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.groupby(['cardio','variable','value']).size().reset_index(name='total')
 
     # 7
-
-
+    chart = sns.catplot(
+        data=df_cat,
+        x='variable',
+        y='total',
+        hue='value',
+        col='cardio',
+        kind='bar'
+    )
 
     # 8
-    fig = None
-
+    fig = chart.fig
 
     # 9
     fig.savefig('catplot.png')
@@ -38,22 +46,36 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 10))
 
     # 15
-
-
+    sns.heatmap(
+        corr,
+        mask=mask,
+        annot=True,        # show correlation coefficients
+        fmt='.1f',         # one decimal place
+        center=0,          # color center at 0
+        vmax=0.3,          # max correlation
+        vmin=-0.1,         # min correlation
+        square=True,
+        linewidths=0.5,
+        ax=ax
+    )
 
     # 16
     fig.savefig('heatmap.png')
